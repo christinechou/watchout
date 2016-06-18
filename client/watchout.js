94c75e292af78e67e93efe37eebb3e54d2359df9
christinechou <christinejchou@gmail.com>
2016-06-17 17:44:00 -0700

    Made mouse component

--- client/watchout.js
+++ client/watchout.js
@@ -1,23 +1,12 @@
 // start slingin' some d3 here.
-// var gameOptions = 
-//   height: 700
-//   width: 700
-//   nEnemies: 30
-//   padding: 20
 
-
-
-// var enemy = function() {
-//   this.age = 5;
-
-// }
 
 
 var svgContainer = d3.select(".board").append("svg")
-                                    .attr("width", 700)
-                                    .attr("height", 600);
+                                    .attr("width", 800)
+                                    .attr("height", 650);
 
-var enemiesID = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+var enemiesID = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 
 var circles = svgContainer.selectAll("circle")
                           .data(enemiesID)
@@ -26,12 +15,8 @@ var circles = svgContainer.selectAll("circle")
 
 //Draw the Circle
 var circleAttributes = circles
-                       .attr("cx", function() {
-                        return Math.random() * 700 - 40;
-                       })
-                       .attr("cy", function() {
-                        return Math.random() * 600 - 40;
-                       })
+                       .attr("cx", function() { return Math.random() * 800 - 40; })
+                       .attr("cy", function() { return Math.random() * 700 - 40; })
                        .attr("r", 15)
                        .style("fill", "red");
 
@@ -39,13 +24,31 @@ var top = Math.random() * 200 - 40;
 var left = Math.random() * 200 - 40;
 
 function move() {
-   circles.transition()
+  circles.transition()
           .duration(1000)
-          .attr('cx', function() { return Math.random() * 700 - 40; })
-          .attr('cy', function() { return Math.random() * 600 - 40; });
+          .attr('cx', function() { return Math.floor(Math.random() * 700) + 40; })
+          .attr('cy', function() { return Math.floor(Math.random() * 600) + 40; });
 }
 
 setInterval(function() { move(); }, 1000);
+
+
+
+var drag = d3.behavior.drag()
+             .on('dragstart', function() { dragCircle.style('fill', 'yellow'); })
+             .on('drag', function() { dragCircle.attr('cx', d3.event.x)
+                                            .attr('cy', d3.event.y); })
+             .on('dragend', function() {dragCircle.style('fill', 'black'); });
+
+var dragCircle = svgContainer.selectAll("mouse")
+                          .data(["05, 30, 15, 10, 25, 30"])
+                          .enter()
+                          .append("polygon")
+                          .attr("points", function(d) { return d; })
+                          .attr("stroke", "blue")
+                          .attr("stroke-width", "2")
+                          .call(drag)
+                          .style("fill", "red");
 
 
 // create a circle and append it to an SVG
